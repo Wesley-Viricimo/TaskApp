72af6df635c91979735d4e95da6d8f1116a81d07
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-11-18 23:47:00 -0300

    Criando TaskService e TaskRepository

---
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
@@ -0,0 +1,10 @@
+package com.devmasterteam.tasks.service.repository
+
+import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
+import com.devmasterteam.tasks.service.repository.remote.TaskService
+
+class TaskRepository {
+    val remote = RetrofitClient.getService(TaskService::class.java)
+
+    
+}
---
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/remote/TaskService.kt
@@ -0,0 +1,46 @@
+package com.devmasterteam.tasks.service.repository.remote
+
+import com.devmasterteam.tasks.service.model.TaskModel
+import retrofit2.Call
+import retrofit2.http.DELETE
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface TaskService {
+
+    @GET("Task")
+    fun list() : Call<List<TaskModel>>
+
+    @GET("Next7Days")
+    fun listNext() : Call<List<TaskModel>>
+
+    @GET("Overdue")
+    fun listOverdue() : Call<List<TaskModel>>
+
+    @GET("Task/{id}")
+    fun load(@Path(value = "id", encoded = true) id : Int) : Call<TaskModel> //Encoded true evita que a url seja quebrada caso houver espaços em branco
+
+    @POST("Task")
+    @FormUrlEncoded //Indica que será enviado um corpo na requisição
+    fun create(@Field("PriorityId") priorityId : Int, @Field("Description") description : String, @Field("DueDate") dueDate : String, @Field("Complete") complete : Boolean) : Call<Boolean>
+
+    @PUT("Task")
+    @FormUrlEncoded //Indica que será enviado um corpo na requisição
+    fun update(@Field("Id") id : Int, @Field("PriorityId") priorityId : Int, @Field("Description") description : String, @Field("DueDate") dueDate : String, @Field("Complete") complete : Boolean) : Call<Boolean>
+
+    @PUT("Task/Complete")
+    @FormUrlEncoded
+    fun complete(@Field("Id") id : Int) : Call<Boolean>
+
+    @PUT("Task/Undo")
+    @FormUrlEncoded
+    fun undo(@Field("Id") id : Int) : Call<Boolean>
+
+    @DELETE("Task")
+    @FormUrlEncoded
+    fun delete (@Field("Id") id : Int) : Call<Boolean>
+}
